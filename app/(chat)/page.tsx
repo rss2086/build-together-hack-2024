f00ac660ef8e22d088ac3097bccfc522b9635a65
Rohan Shah <rss2086@gmail.com>
2024-06-08 08:09:49 -0700

    initial boilerplate ui + landing

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -4,6 +4,10 @@ import { AI } from '@/lib/chat/actions'
 import { auth } from '@/auth'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
+import { ThemeToggle } from '@/components/theme-toggle'
+import Globe from '@/components/magicui/globe'
+import Link from 'next/link'
+import { Input } from '@/components/ui/input'
 
 export const metadata = {
   title: 'Next.js AI Chatbot'
@@ -15,8 +19,86 @@ export default async function IndexPage() {
   const missingKeys = await getMissingKeys()
 
   return (
-    <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} session={session} missingKeys={missingKeys} />
-    </AI>
+    <main className="w-full">
+      <header className="flex flex-row-reverse w-full justify-between p-4">
+        <ThemeToggle />
+        {/* <Chat id={id} session={session} missingKeys={missingKeys} /> */}
+      </header>
+      <section>
+        <h1 className="text-5xl text-center font-serif">
+          Explained in five levels
+        </h1>
+        {/* <div className="mt-12 bg-green-400"> */}
+        <Globe className="mt-36" />
+        <Link
+          className="absolute top-1/4 left-[27%] hover:bg-gray-200 px-4 py-2"
+          href="/test"
+        >
+          <h1 className="text-xl text-[#36C] font-bold">English</h1>
+          <h2 className="text-sm">2,001,312+ articles</h2>
+        </Link>
+        <Link
+          className="absolute top-[40%] left-[22%] hover:bg-gray-200 px-4 py-2"
+          href="/test"
+        >
+          <h1 className="text-xl text-[#36C] font-bold">Spanish</h1>
+          <h2 className="text-sm">2,101,312+ articles</h2>
+        </Link>
+        <Link
+          className="absolute top-[55%] left-[22%] hover:bg-gray-200 px-4 py-2"
+          href="/test"
+        >
+          <h1 className="text-xl text-[#36C] font-bold">French</h1>
+          <h2 className="text-sm">2,101,312+ articles</h2>
+        </Link>
+        <Link
+          className="absolute top-[70%] left-[27%] hover:bg-gray-200 px-4 py-2"
+          href="/test"
+        >
+          <h1 className="text-xl text-[#36C] font-bold">Japanese</h1>
+          <h2 className="text-sm">2,101,312+ articles</h2>
+        </Link>
+        <Link
+          className="absolute top-1/4 right-[27%] hover:bg-gray-200 px-4 py-2"
+          href="/test"
+        >
+          <h1 className="text-xl text-[#36C] font-bold">Mandarin</h1>
+          <h2 className="text-sm">2,101,312+ articles</h2>
+        </Link>
+        <Link
+          className="absolute top-[40%] right-[22%] hover:bg-gray-200 px-4 py-2"
+          href="/test"
+        >
+          <h1 className="text-xl text-[#36C] font-bold">Hindi</h1>
+          <h2 className="text-sm">2,101,312+ articles</h2>
+        </Link>
+        <Link
+          className="absolute top-[55%] right-[22%] hover:bg-gray-200 px-4 py-2"
+          href="/test"
+        >
+          <h1 className="text-xl text-[#36C] font-bold">Gujurati</h1>
+          <h2 className="text-sm">2,101,312+ articles</h2>
+        </Link>
+        <Link
+          className="absolute top-[70%] right-[27%] hover:bg-gray-200 px-4 py-2"
+          href="/test"
+        >
+          <h1 className="text-xl text-[#36C] font-bold">Portugese</h1>
+          <h2 className="text-sm">2,101,312+ articles</h2>
+        </Link>
+        <div className="absolute top-[85%] left-[20%] w-full">
+          <h1 className="py-2 font-bold text-2xl font-serif">
+            Search for any topic
+          </h1>
+          <Input
+            className=" w-full max-w-4xl py-6"
+            placeholder="Search for a language"
+          />
+        </div>
+      </section>
+    </main>
+    // <AI initialAIState={{ chatId: id, messages: [] }}>
+    //   <Chat id={id} session={session} missingKeys={missingKeys} />
+    // </AI>
   )
 }
